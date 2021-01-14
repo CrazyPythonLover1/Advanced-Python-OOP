0984c46f94581d0e5af12095d8d79b6c19953aee
Md. Mainul Islam (Faruqi) <programmermainul@gmail.com>
2021-01-14 17:26:15 +0000

    Python Object

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 class PlayerCharacter: 
-  def __init__(self, name):
+  def __init__(self, name, age):
     self.name = name
+    self.age = age
 
   def run(self):
     print('run')
 
-player1 = PlayerCharacter('Mainul')
-player2 = PlayerCharacter('Tommy')
+player1 = PlayerCharacter('Mainul', 24)
+player2 = PlayerCharacter('Tommy', 19)
 
 print(player1.name)
 print(player2.name)
+print(player1.age)
+print(player2.age)
