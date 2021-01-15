292bac21e779611f69d4bb8c95438dc0d767b4b2
Md. Mainul Islam (Faruqi) <programmermainul@gmail.com>
2021-01-15 10:13:50 +0000

    Note Encapsulation, abstraction, inheritace

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Advanced-Python-OOP
 Complete python Developer 2020 Zero to Mastery
+
+## 4 PILLARS OF OOP
+
+1. ENCAPSULATION
+> We can protect unexpected access from outside
+> Inner variable of a class can be accessed in a inner function(method)
+> Inner variable and function can not be accessed outer function
+
+2. ABSTRACTION
+> Handle complexity by hiding unnecessary details from the users
+> User don't worry about it's codding 
+> User just use it 
+
+3. INHERITANCE 
+> We can inherit the class in any class 
+
+4. 
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 class PlayerCharacter: 
+  # Class Object Attribute
+  membership = True
   def __init__(self, name, age):
-    self.name = name
+    self.name = name # Attribute
     self.age = age
 
+  # Method
   def run(self):
-    print('run')
+    print(f'{self.name} run fast')
+
+  @classmethod
+  def cls_method(cls, param1, param2):
+    #Code 
+    return
+  @staticmethod
+  def stc_method(param1, param2):
+    #Code
+    return
+
 
 player1 = PlayerCharacter('Mainul', 24)
 player2 = PlayerCharacter('Tommy', 19)
 
-print(player1.name)
+print(player1.run())
+print(player1.membership)
 print(player2.name)
 print(player1.age)
 print(player2.age)
